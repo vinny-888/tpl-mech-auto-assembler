4fd04d6f732ac9f0e113ebfd1eda2137360000c6
Vinny <>
2023-01-17 22:59:08 -0800

    fix bugs

--- js/util/utils.js
+++ js/util/utils.js
@@ -11,7 +11,7 @@ function partsRevealedImage(part, model, style) {
 
 function fullRevealedImage(style) {
     if(style == 'CAMM-E'){
-        return '<img height="60px" src="https://cb-media.sfo3.cdn.digitaloceanspaces.com/mechs/templates/' + style.toLowerCase().replaceAll(' ', '-') + '.webp" title="'+[style].join(' ')+'" />';
+        return '<img height="60px" src="https://cb-media.sfo3.cdn.digitaloceanspaces.com/mechs/templates/camm-e.webp" title="'+[style].join(' ')+'" />';
     } else {
         return '<img height="60px" src="https://cb-media.sfo3.cdn.digitaloceanspaces.com/mechs/templates/' + style.toLowerCase().replaceAll(' ', '-').replaceAll('-', '_') + '.webp" title="'+[style].join(' ')+'" />';
     }
