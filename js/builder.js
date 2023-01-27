b1d469f76ab065da666aee844df4d421fb43f5cb
Vinny <>
2023-01-26 16:34:00 -0800

    add stats

--- js/builder.js
+++ js/builder.js
@@ -13,7 +13,7 @@ window.addEventListener('load', async () => {
 });
 
 function initDropdowns(){
-  // let engine = document.getElementById('engine_style');
+  let engine = document.getElementById('engine_style');
   let head = document.getElementById('head_style');
   let body = document.getElementById('body_style');
   let legs = document.getElementById('legs_style');
@@ -22,16 +22,15 @@ function initDropdowns(){
   
   Object.keys(meta_parts).forEach((key, index)=>{
     let metadata = meta_parts[key];
-    // let model = metadata.find((att)=> att.trait_type == 'Model').value;
+    let model = metadata.find((att)=> att.trait_type == 'Model').value;
     let style = metadata.find((att)=> att.trait_type == 'Style').value;
     let part = metadata.find((att)=> att.trait_type == 'Part').value;
 
     let html = '<option value="'+(style)+'" '+(index == 0 ? 'selected' : '')+'>'+(style)+'</option>';
 
-    // if(part == 'Engine'){
-    //   engine.innerHTML += html;
-    // } else 
-    if(part == 'Head'){
+    if(part == 'Engine'){
+      engine.innerHTML += html;
+    } else if(part == 'Head'){
       head.innerHTML += html;
     } else if(part == 'Body'){
       body.innerHTML += html;
@@ -45,14 +44,14 @@ function initDropdowns(){
 }
 
 function addEventlisteners(){
-  // let engine = document.getElementById('engine_style');
+  let engine = document.getElementById('engine_style');
   let head = document.getElementById('head_style');
   let body = document.getElementById('body_style');
   let legs = document.getElementById('legs_style');
   let left_arm = document.getElementById('left_arm_style');
   let right_arm = document.getElementById('right_arm_style');
 
-  // engine.onchange = updatePreview;
+  engine.onchange = updatePreview;
   head.onchange = updatePreview;
   body.onchange = updatePreview;
   legs.onchange = updatePreview;
@@ -61,19 +60,50 @@ function addEventlisteners(){
 }
 
 function updatePreview(){
-  // let engine = document.getElementById('engine_style');
+  let engine = document.getElementById('engine_style');
   let head = document.getElementById('head_style');
   let body = document.getElementById('body_style');
   let legs = document.getElementById('legs_style');
   let left_arm = document.getElementById('left_arm_style');
   let right_arm = document.getElementById('right_arm_style');
 
-  // let engine_style = engine.value;
+  let engine_style = engine.value;
   let head_style = head.value;
   let body_style = body.value;
   let legs_style = legs.value;
   let left_arm_style = left_arm.value;
   let right_arm_style = right_arm.value;
+
+  let enduranceTotal = 0;
+  let speedTotal = 0;
+  let powerTotal = 0;
+  Object.keys(meta_parts).forEach((key, index)=>{
+    let metadata = meta_parts[key];
+    let model = metadata.find((att)=> att.trait_type == 'Model').value;
+    let style = metadata.find((att)=> att.trait_type == 'Style').value;
+    let part = metadata.find((att)=> att.trait_type == 'Part').value;
+    let endurance = metadata.find((att)=> att.trait_type == 'Endurance').value;
+    let speed = metadata.find((att)=> att.trait_type == 'Speed').value;
+    let power = metadata.find((att)=> att.trait_type == 'Power').value;
+
+    if((part == 'Engine' && engine_style == style) || 
+      (part == 'Head' && head_style == style) ||
+      (part == 'Body' && body_style == style) ||
+      (part == 'Legs' && legs_style == style) ||
+      (part == 'Arm' && left_arm_style == style) ||
+      (part == 'Arm' && right_arm_style == style)){
+      enduranceTotal+=endurance;
+      speedTotal+=speed;
+      powerTotal+=power;
+    }
+  });
+
+  
+  document.getElementById('endurance_total').innerHTML = enduranceTotal;
+  document.getElementById('speed_total').innerHTML = speedTotal;
+  document.getElementById('power_total').innerHTML = powerTotal;
+  document.getElementById('stats_total').innerHTML = enduranceTotal + speedTotal + powerTotal;
+
   showPreview(head_style, body_style, legs_style, left_arm_style, right_arm_style);
 }
 
